140fce76a036358ce7d6788723847ede9a75d983
IchigoArndt <arndtfelipe@gmail.com>
2018-12-19 12:45:08 -0200

    Commit inicial

---
+++ TelaAleatorizador/TelaAleatorizador.Apresentacao/Principal.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TelaAleatorizador.Apresentacao
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.txtSaida = new System.Windows.Forms.TextBox();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Caminho Entrada";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Caminho Saida";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantidade";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(107, 29);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(155, 20);
+            this.txtEntrada.TabIndex = 3;
+            // 
+            // txtSaida
+            // 
+            this.txtSaida.Location = new System.Drawing.Point(107, 130);
+            this.txtSaida.Name = "txtSaida";
+            this.txtSaida.Size = new System.Drawing.Size(155, 20);
+            this.txtSaida.TabIndex = 4;
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Enabled = false;
+            this.txtQtd.Location = new System.Drawing.Point(107, 81);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(155, 20);
+            this.txtQtd.TabIndex = 5;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(16, 171);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(107, 23);
+            this.btnValidar.TabIndex = 6;
+            this.btnValidar.Text = "Validar Caminhos";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(187, 171);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 7;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 219);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtQtd);
+            this.Controls.Add(this.txtSaida);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Principal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.TextBox txtSaida;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Button btnIniciar;
+    }
+}
+
---
+++ TelaAleatorizador/TelaAleatorizador.Apresentacao/Principal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelaAleatorizador.Apresentacao
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+            btnIniciar.Enabled = false;
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            string caminhoEntrada = txtEntrada.Text;
+            string caminhoSaida = txtSaida.Text;
+
+            DirectoryInfo di = new DirectoryInfo(caminhoEntrada);
+            DirectoryInfo ds = new DirectoryInfo(caminhoSaida);
+            if(di.Exists && ds.Exists)
+            {
+                if(di != ds)
+                {
+                    MessageBox.Show("Caminhos Invalidos");
+                }
+                else
+                {
+                    MessageBox.Show("Caminho Validos");
+                    btnIniciar.Enabled = true;
+                }
+            }else
+            {
+                MessageBox.Show("Caminhos Invalidos");
+            }
+        }
+    }
+}
